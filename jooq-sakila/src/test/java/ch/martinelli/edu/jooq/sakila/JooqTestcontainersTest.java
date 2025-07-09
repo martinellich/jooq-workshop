18ffdb60677855fa01443a630db8c5489dd81741
Simon Martinelli <simon@martinelli.ch>
2025-07-09 12:30:21 +0200

    Optimize imports

--- jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/JooqTestcontainersTest.java
+++ jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/JooqTestcontainersTest.java
@@ -6,11 +6,6 @@ import org.jooq.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
